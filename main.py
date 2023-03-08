093c5786af1c017e4901899adabf08e795842632
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2023-03-07 20:02:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ for i in range(1, 20):   # Loop through the first 3 pages
     # Loop through each event card and extract the desired data
     for j, card in enumerate(event_cards):
         event_name = card.find('div', {'class': 'eds-event-card__formatted-name--is-clamped eds-event-card__formatted-name--is-clamped-three eds-text-weight--heavy'}).text.strip()
-        event_date = card.find('div', {'class': 'eds-event-card-content__sub-title eds-text-color--primary-brand eds-l-pad-bot-1 eds-l-pad-top-2 eds-text-weight--heavy eds-text-bm'}).text.strip().split('+')[0]
+        event_organizer_elem = card.find('div', {'data-subcontent-key': 'organizerName'})
+        event_organizer = event_organizer_elem.text.strip() if event_organizer_elem is not None else 'N/A'
+        event_date_elem = card.find('div', {'class': 'eds-event-card-content__sub-title eds-text-color--primary-brand eds-l-pad-bot-1 eds-l-pad-top-2 eds-text-weight--heavy eds-text-bm'})
+        event_date = event_date_elem.text.strip().split('(')[0] if event_date_elem is not None else 'N/A'
         event_url_elem = card.find('a', {'class': 'eds-event-card-content__action-link'})
         event_url = event_url_elem['href'] 
 
@@ -31,12 +34,13 @@ for i in range(1, 20):   # Loop through the first 3 pages
         # print('Event Name:', event_name)
         # print('Event Date:', event_date)
         # print('Event Url:', event_url)
-       # print('\n')
+        # print('\n')
 
         # Create a dictionary of the event data and append it to the event_data list
         event_dict = {
             'id': id_counter,
             'name': event_name,
+            'host': event_organizer,
             'date': event_date,
             'url': event_url
         }
